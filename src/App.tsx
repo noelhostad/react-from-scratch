c62bc32c315be7bc10b679d8222ec01a86f92527
Noel Hostad <noel@juice-e.co.uk>
2026-01-04 15:05:52 +0000

    completed 18

--- src/App.tsx
+++ src/App.tsx
@@ -23,13 +23,15 @@ export function App() {
 
 function Main() {
   const [liked, setLiked] = useState<Puppy["id"][]>([1, 3]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
   return (
     <main>
       <div className="mt-24 grid gap-8 sm:grid-cols-2">
-        <Search />
+        <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
         <Shortlist puppies={puppies} liked={liked} setLiked={setLiked} />
       </div>
-      <PuppiesList puppies={puppies} liked={liked} setLiked={setLiked} />
+      <PuppiesList puppies={puppies} liked={liked} setLiked={setLiked} searchQuery={searchQuery} />
       <NewPuppyForm />
     </main>
   );
--- src/components/PuppiesList.tsx
+++ src/components/PuppiesList.tsx
@@ -6,21 +6,27 @@ export function PuppiesList({
   puppies,
   liked,
   setLiked,
+  searchQuery
 }: {
   puppies: Puppy[];
   liked: Puppy["id"][];
   setLiked: Dispatch<SetStateAction<Puppy["id"][]>>;
+  searchQuery: string;
 }) {
   return (
     <ul className="mt-8 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {puppies.map((puppy) => (
-        <PuppyCard
-          key={puppy.id}
-          puppy={puppy}
-          liked={liked}
-          setLiked={setLiked}
-        />
-      ))}
+      {
+        puppies
+        .filter(puppy => puppy.vibe.toLowerCase().includes(searchQuery.toLowerCase()))
+        .map((puppy) => (
+            <PuppyCard
+            key={puppy.id}
+            puppy={puppy}
+            liked={liked}
+            setLiked={setLiked}
+            />
+        ))
+      }
     </ul>
   );
 }
